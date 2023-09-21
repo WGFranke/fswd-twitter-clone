03b6def114cebad2bf88756d9870c1a6d876532f
wfranke <wfranke@DESKTOP-1GDPVQA>
2023-09-21 09:33:24 -0500

    push for Harry's help

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,10 +3,9 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
-import "jquery"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
 
 import 'bootstrap/dist/js/bootstrap.bundle'
 
